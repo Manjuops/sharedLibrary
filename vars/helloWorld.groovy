c901999804f0ff52a28ed4dbac20419dbae3c785
Manjuops <manjunath.hegde@opstree.com>
2024-10-04 11:05:02 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,3 @@
 def call() {
-    sh ' Hello manju, You are capable of doing anything. Things will definately come to you what you wish for.Be confident. This time will pass'
+    sh 'echo "Hello manju, You are capable of doing anything. Things will definately come to you what you wish for.Be confident. This time will pass"'
 }
